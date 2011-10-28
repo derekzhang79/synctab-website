bca2c3eb36fb922b3bc22c3d41c532a969d164f3
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2011-10-28 18:25:25 +0300

    Extract send mail to separate method

--- website/mail.py
+++ website/mail.py
@@ -1,5 +1,3 @@
-__author__ = 'ruslan'
-
 import smtplib
 import datetime
 from django.conf import settings
@@ -9,20 +7,28 @@ def send_contact_message(name, email, message):
     from_addr = settings.CONTACT_FROM
     now = datetime.datetime.now()
     subject = settings.CONTACT_SUBJECT % (now.strftime("%Y-%b-%d %H:%M:%S"))
+    
+    msg = "Sender Name:\n%s\n\nEmail:\n%s\n\nContact Message:\n%s" % (name, email, message)
+    return send_email(from_addr, to_addr, email, subject, msg)
 
+
+def send_email(from_addr, to_addr, reply_to_addr, subject, message):
     server = None
     try:
-        msg = "From: %s\nReply-To: %s\nTo: %s\nSubject: %s\n\n" % (from_addr, email, to_addr, subject)
-        msg += "Sender Name:\n%s\n\nEmail:\n%s\n\nContact Message:\n%s" % (name, email, message)
+        msg = ("From: %s\n" +
+               "Reply-To: %s\n" +
+               "To: %s\nSubject: %s\n\n") % \
+              (from_addr, reply_to_addr, to_addr, subject)
+        
+        msg += message
 
         server = smtplib.SMTP(settings.MAIL_SERVER)
         server.sendmail(from_addr, to_addr, msg)
 
         return True
     except smtplib.SMTPException:
-        print "Error to send contact email from %s %s with message %s" % (name, email, message)
+        print "Error to send contact email %s" % message
         return False
     finally:
         if server is not None:
             server.quit()
-  
